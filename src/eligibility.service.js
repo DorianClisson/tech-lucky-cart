eae8240663fa95d0ddd747782e23fb64f9cf3550
DorianClisson <dorian.clisson@gmail.com>
2024-06-28 20:11:40 +0200

    checkCriteria function + comparison map

--- src/eligibility.service.js
+++ src/eligibility.service.js
@@ -1,3 +1,13 @@
+/**
+ * Get a nested value in object
+ * path written in "pathA.pathB.pathC" format
+ * No index for array because we need to iterate through each array elem to later check if one is correct
+ * If an array is found while getting the nested value then should return an array of results (with the final value corresponding to the rest of path for each array element)
+ *
+ * @param cart
+ * @param path
+ * @return {any}
+ */
 const getPathValue = (cart, path) => {
     const splittedPath = path.split(".");
     return splittedPath.reduce((actualValue, key) => {
@@ -7,8 +17,37 @@ const getPathValue = (cart, path) => {
     }, cart);
 };
 
+// map of each comparison and its maths logic for easy access
+const CRITERIA_COMPARATORS = {
+    eq: (cartValue, criteriaValue) => cartValue == criteriaValue,
+    // TODO all others
+    //gt
+    //gte
+    //lt
+    //lte
+    //in
+    //and
+    //or
+};
+
+/**
+ * Check if a single criteria is fulfilled (= true)
+ *
+ * @param cartValue
+ * @param criteriaData
+ * @return {boolean}
+ */
 const checkCriteria = (cartValue, criteriaData) => {
-    return false; // TODO
+    const isCriteriaDataObject = typeof criteriaData === "object";
+
+    const criteriaOperator = isCriteriaDataObject
+        ? Object.keys(criteriaData)[0]
+        : "eq"; // when eq comparison => value is explicitly given without "eq" written in an object format
+    const criteriaValue = isCriteriaDataObject
+        ? criteriaData[criteriaOperator]
+        : criteriaData; // if criteriaData is not an object then it is "eq" comparison therefore the value is directly given so criteriaValue = criteriaData
+
+    return CRITERIA_COMPARATORS[criteriaOperator](cartValue, criteriaValue);
 };
 
 class EligibilityService {
