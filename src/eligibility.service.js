387f0b7d851d6fb4b026f89272e01b7df53b9d71
DorianClisson <dorian.clisson@gmail.com>
2024-06-28 19:56:00 +0200

    init isEligible logic + get cart value for each criteria

--- src/eligibility.service.js
+++ src/eligibility.service.js
@@ -1,18 +1,35 @@
+const getPathValue = (cart, path) => {
+    const splittedPath = path.split(".");
+    return splittedPath.reduce((actualValue, key) => {
+        return Array.isArray(actualValue)
+            ? actualValue.map((arraySubValue) => arraySubValue[key])
+            : actualValue[key];
+    }, cart);
+};
+
+const checkCriteria = (cartValue, criteriaData) => {
+    return false; // TODO
+};
+
 class EligibilityService {
-  /**
-   * Compare cart data with criteria to compute eligibility.
-   * If all criteria are fulfilled then the cart is eligible (return true).
-   *
-   * @param cart
-   * @param criteria
-   * @return {boolean}
-   */
-  isEligible(cart, criteria) {
-    // TODO: compute cart eligibility here.
-    return false;
-  }
+    /**
+     * Compare cart data with criteria to compute eligibility.
+     * If all criteria are fulfilled then the cart is eligible (return true).
+     *
+     * @param cart
+     * @param criteria
+     * @return {boolean}
+     */
+    isEligible(cart, criteria) {
+        const criteriaKeys = Object.keys(criteria);
+        return criteriaKeys.every((criteriaKey) => {
+            const correspondingValueInCart = getPathValue(cart, criteriaKey);
+            const criteriaData = criteria[criteriaKey];
+            return checkCriteria(correspondingValueInCart, criteriaData);
+        });
+    }
 }
 
 module.exports = {
-  EligibilityService,
+    EligibilityService,
 };
